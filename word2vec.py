14bd90b8f6db40f31ec24ee8f548180bcc726e6c
Tetiana Klymchuk <missklimchuck@yandex.ua>
2021-10-23 23:35:25 +0200

    [NEW] Added Embedding Class definition

---
+++ word2vec.py
@@ -0,0 +1,63 @@
+from gensim.models.word2vec import Word2Vec
+from gensim.models import KeyedVectors
+import string
+import re
+from tqdm import tqdm
+
+from gensim.test.utils import get_tmpfile
+import os
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+import pandas as pd
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import numpy as np
+
+from transformers import BertTokenizer, BertModel
+import torch
+
+from utils.utils import *
+
+
+class Embeddings:
+    """
+    Class that handles word embeddings
+    """
+
+    def __init__(self, df=None, path=None, column='text', embedding_dim=100, embedding_window=5, min_count=100):
+        """
+        :param df:  dataframe with coluns specified
+        :param path: path to pretrained embeddings.
+        :param column: either abstract or text (default: text) <str>
+        :param embedding_dim: word vectors dimension.
+        :param embedding_window: window size to compute embeddings.
+        :param min_count: minimum frequency of a word in the corpus to consider it.
+        """
+        if path is None and df is None:
+            raise Exception('You must provide either a df or a path')
+        if df is None:
+            self.load(path)
+        else:
+            model = self._get_embeddings(df, column, embedding_dim, embedding_window, min_count)
+            self.wv = model.wv
+
+        self.embedding_dim = embedding_dim
+        self.cluster_model = None
+
+    def _get_embeddings(self):
+        pass
+
+    def most_similar(self):
+        pass
+
+    def get_embedding(self):
+        pass
+
+    def get_sentence_embedding(self):
+        pass
+
+    def plot_embedding(self):
+        pass
+
+    def plot_cluster(self):
+        pass 
