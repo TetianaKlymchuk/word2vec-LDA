08e9d19080ad8167292948ade7c066c9db8adfe2
Tetiana Klymchuk <missklimchuck@yandex.ua>
2021-10-23 23:42:38 +0200

    [NEW] Added plot_sentence_embedding function

--- word2vec.py
+++ word2vec.py
@@ -135,5 +135,99 @@ class Embeddings:
 
         plt.show()
 
+    def plot_sentence_embeddings(self, original, clusters=None):
+
+        # GET SENTENCE EMBEDDINGS
+        tweet_embeddings = []
+        for text in original['text<gx:text>']:
+            emb = self.get_sentence_embedding(text.strip())
+            if emb is not None:
+                tweet_embeddings.append(emb)
+            if emb is None:
+                tweet_embeddings.append([0] * self.embedding_dim)
+
+        tweet_embeddings = np.array(tweet_embeddings)
+
+        # REDUCE DIMENSIONALITY TO PLOT
+        tsne = TSNE(n_components=2, random_state=0)
+        X_tsne = tsne.fit_transform(tweet_embeddings)
+
+        df = pd.DataFrame(X_tsne, columns=['x', 'y'])
+
+        fig = plt.figure()
+
+        ax = fig.add_subplot(1, 1, 1)
+
+        # CHANGE COLOR OF THE DATA POINT
+        tot_col = []
+        if clusters is None:
+            tot_col = ['blue'] * len(df)
+        else:
+            tot_col = clusters
+
+        ax.scatter(df['x'], df['y'], c=tot_col)
+
+        original = original.reset_index()  # Needed to match the df indices.
+
+        def onclick(event):
+
+            ax.clear()
+
+            # CHECK POINT NEAREST TO CURSOR CLICK
+            min = 3
+            minx = 3
+            miny = 4
+            index = None
+            for word, pos in df.iterrows():
+                x_dif = pos.x - event.xdata
+                y_dif = pos.y - event.ydata
+                dif = np.sqrt(x_dif ** 2 + y_dif ** 2)
+                if dif < min:
+                    minx = x_dif
+                    miny = y_dif
+                    min = dif
+                    index = word
+
+            # GET THE TWEET AND SPLIT EVERY 150 TO FIT INTO THE PLOT
+            old_tweet = original['text<gx:text>'][index]
+            split_every = 150
+
+            last_i = 0
+            tweet = ''
+            if len(old_tweet) <= split_every:
+                tweet = old_tweet
+            else:
+                for i in range(split_every, len(old_tweet), split_every):
+                    tweet += old_tweet[last_i:i] + '\n'
+                    last_i = i
+
+                tweet += old_tweet[last_i:] + '\n'
+
+            tweet = tweet.strip()
+
+            # SET THE TWEET AS THE TITLE
+            fig.suptitle(tweet, fontsize=12)
+
+            # CHANGE COLOR OF THE DATA POINT
+            if clusters is None:
+                tot_col = []
+                for m in range(len(df)):
+                    if m == index:
+                        tot_col.append('orange')
+                    else:
+                        tot_col.append('blue')
+            else:
+                tot_col = clusters
+                tot_col[index] = 213
+
+            ax.scatter(df['x'], df['y'], c=tot_col)
+
+            plt.gcf().canvas.draw_idle()
+
+        fig.canvas.mpl_connect('button_press_event', onclick)
+
+        start = None
+        end = None
+
     def plot_cluster(self):
         pass 
